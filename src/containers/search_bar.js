9486228b5996f237ab797428c51f54241a9639f6
Praffulsingh9 <prafful.singh017@gmail.com>
2018-10-23 12:35:59 +0530

    Form handling is added to SearchBar Component

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            term : ''
+        }
+    }
+
+    onInputChange = (event) => {
+        this.setState({term:event.target.value})
+        }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form className="input-group">
-                <input/>
+            <form 
+            className="input-group" 
+            style={{margin:"20px"}}
+            onSubmit={(event)=>this.onFormSubmit(event)}>
+                <input 
+                placeholder="Get a five day forecast of your city"
+                className="form-control"
+                value={this.state.term}
+                onChange={(event) => this.onInputChange(event)}/>
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Search</button>
                 </span>
