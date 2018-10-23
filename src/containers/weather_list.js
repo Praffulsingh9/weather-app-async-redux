0591d515aa3d3f65729e56e80de98cf6b2c28d92
Praffulsingh9 <prafful.singh017@gmail.com>
2018-10-23 22:03:05 +0530

    Weather App Completed

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import Chart from '../components/chart';
+
+
 class WeatherList extends Component {
     
     renderWeather(cityData){
@@ -8,6 +10,8 @@ class WeatherList extends Component {
         const temps = cityData.list.map(weather=>weather.main.temp)
         const pressures = cityData.list.map(weather=>weather.main.pressure)
         const humidities = cityData.list.map(weather=>weather.main.humidity)
+        
+        
 
         return(
             <tr key={name}>
